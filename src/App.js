1d314a1418268394815ac7a916f4371607171489
Courtney Sims <courtney.sims@wpengine.com>
2023-08-22 15:37:12 -0500

    added images and set more props for cards

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import MainContent from './Components/MainContent';
 import Card from './Components/Card';
 import Footer from './Components/Footer';
 import Aurora from "./images/aurorasitalianbistro.png"
+import BallPit from "./images/ballpit.jpg"
+import Ribbon from "./images/ribbon.png"
 
 export default function App(){
     return (
@@ -19,11 +21,28 @@ export default function App(){
                     numReviews="6"
                     location="USA"
                     name="World's Best Meatball from Aurora's Italian Bistro"
-                    price="136" 
+                    price="136"
+                    currency="Jewels" 
+                    alt="Chef Aurora of Aurora's Italian Bistro"
                 />
-                <Card />
-                <Card />
-                <Card />
+                <Card 
+                    img={BallPit}
+                    rating="5.0"
+                    numReviews="10"
+                    location="USA"
+                    name="Penny the Puppy's Ball Pit Experience"
+                    price="1"
+                    currency="Kong Ball"
+                    alt="Image from dogpools.shop"
+                />
+                <Card 
+                    img={Ribbon}
+                    rating="5.0"
+                    numReviews="5000"
+                    name="Very Long Ribbons R Us"
+                    price="1"
+                    currency="Floor scoop"
+                    alt="Image from Ylanite Koppens @ Pexels"/>
             </div>
             <Footer />
         </div>
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ import Star from "../images/star.png"
 export default function Card(props){
     return (
         <div className='card'>
-            <img className='card--bistroImg' src={props.img} alt="Chef Aurora of Aurora's Italian Bistro"></img>
+            <img className='card--bistroImg' src={props.img} alt={props.alt}></img>
             <div className='card--bistroStats'>
                 <img className='card--star' src={Star} alt="Neon star"></img>
                 <span>{props.rating}</span>
@@ -12,7 +12,7 @@ export default function Card(props){
                 <span className='gray'>{props.location}</span>
             </div>
                 <p>{props.name}</p>
-                <p><b>From ${props.price}</b> / person</p>
+                <p><b>From {props.price} {props.currency}</b> / person</p>
             
         </div>
     )
